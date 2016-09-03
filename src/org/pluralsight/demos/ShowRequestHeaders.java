80c765a8fd5064b2308ba540f7f507425ce1613f
Rajasekhar Karanam <karanam.rajasekhar44@gmail.com>
2016-09-04 04:53:56 +0530

    Prints all HTTP Request Headers

---
+++ src/org/pluralsight/demos/ShowRequestHeaders.java
@@ -0,0 +1,50 @@
+package org.pluralsight.demos;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ShowRequestHeaders")
+public class ShowRequestHeaders extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ShowRequestHeaders() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<!DOCTYPE html>");
+		out.println("	<head>");
+		out.println("		<title>All Request Headers</title>");
+		out.println("	</head>");
+		out.println("	<body>");
+		out.println("		<h3>All Request Headers</h3>");
+		out.println("		<p><b>Request Method: </b>" + request.getMethod() + "</p>");
+		out.println("		<p><b>Request URI: </b>" + request.getRequestURI() + "</p>");
+		out.println("		<p><b>Request Protocol: </b>" + request.getProtocol() + "</p>");
+		
+		Enumeration<String> headers = request.getHeaderNames();
+		while(headers.hasMoreElements()) {
+			String headerName = headers.nextElement();
+			out.println("<p><b>" + headerName + "</b>: " + request.getHeader(headerName) + "</p>");
+		}
+		
+		out.println("	</body>");
+		out.println("</html>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+	}
+
+}
