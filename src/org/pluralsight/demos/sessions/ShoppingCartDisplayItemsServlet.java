97e1d96e6b585be8ae6ab45b70569371e7809f26
rkaranam <karanam.rajasekhar44@gmail.com>
2016-09-09 21:30:06 +0530

    Display Items for Shopping Cart

--- src/org/pluralsight/demos/sessions/ShoppingCartDisplayItemsServlet.java
+++ src/org/pluralsight/demos/sessions/ShoppingCartDisplayItemsServlet.java
@@ -1,40 +1,60 @@
 package org.pluralsight.demos.sessions;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ShoppingCartDisplayItemsServlet
- */
 @WebServlet("/ShoppingCartDisplayItemsServlet")
 public class ShoppingCartDisplayItemsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public ShoppingCartDisplayItemsServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath()).append(request.getServletPath());
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		Map<String, Integer> items = new HashMap<>();
+		items.put("Reebok Shoes", 650);
+		items.put("Skykar Bag", 780);
+		items.put("Lenovo Backpack", 1390);
+		items.put("B50-70 Laptop Charger", 2800);
+		
+		out.println("<!DOCTYPE html>");
+		out.println("   <head>");
+		out.println("       <title>Items Catalog</title>");
+		out.println("   </head>");
+		out.println("   <body>");
+		out.println("		<h2>Items Catalog</h2>");
+		out.println("		<table name='items' cellpadding='5' cellspacing='5'>");
+		out.println("			<tr><th>S.No.</th><th>Item</th><th>Price</th><th>Quantity</th></tr>");
+		
+		int i = 0;
+		for (Map.Entry<String, Integer> item : items.entrySet()) {
+			out.println("<tr><td>" + (++i) + "</td><td>" + item.getKey() + "</td><td>" + item.getValue() 
+						+ "</td><td><input type='button' name='addToCart' value='Add To Cart'/></td></tr></p>");			
+		}
+		out.println("		</table>");
+		out.println("		<p></p>");
+		out.println("   </body>");
+		out.println("</html>");
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
